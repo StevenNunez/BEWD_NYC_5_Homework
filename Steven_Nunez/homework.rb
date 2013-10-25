ff10191355798e4f05b033028800cca01e9107ea
Steven Nunez <snunez@cyrusinnovation.com>
2013-10-24 22:00:05 -0400

    Finish homework 1

---
+++ Steven_Nunez/homework.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+
+class Person
+end
+
+describe Person do
+  it "is initialized with a name and profession" do
+    person = Person.new "Steven", "Software Developer"
+    person.introduce.must_equal "Hi! My name is Steven. I'm a Software Developer"
+    person.introduce_from_across_the_room.must_equal "HI! MY NAME IS STEVEN. I'M A SOFTWARE DEVELOPER"
+  end
+end
